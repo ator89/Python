22598d72099fa4659b8868a382f168c7f7499119
antox89 <ator89@outlook.com>
2019-09-19 17:51:09 -0600

    week3: list functions

--- MITx_Python/week3/list.py
+++ MITx_Python/week3/list.py
@@ -6,9 +6,32 @@ l2 = [7,9,1]
 
 l3 = l + l2
 
+#sort without mutate
+print(sorted(l3))
+
+#delete by index
 del(l2[1])
 
 print(l2)
+
+#pop
 l3.pop()
+
+#remove key
 l3.remove(9)
 print(l3)
+
+
+s = 'hola'
+
+print(list(s))
+
+nombre = 'angel'
+nombre.split('g')
+print(nombre.split('g'))
+
+print('__'.join(s))
+
+#mutates
+#l.reverse()
+#l.sort()
