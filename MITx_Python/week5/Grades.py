6a91d799cf3eb213715294e0492060587d7857ab
antox89 <ator89@outlook.com>
2019-10-11 18:28:24 -0600

    week5: Grades

--- MITx_Python/week5/Grades.py
+++ MITx_Python/week5/Grades.py
@@ -5,6 +5,7 @@ class Grades(object):
         self.students = []
         self.grades = {}
         self.isSorted = True
+
     def addStudent(self, student):
         """Assumes: student is of type Student
         Add student to the grade book"""
@@ -13,22 +14,49 @@ class Grades(object):
         self.students.append(student)
         self.grades[student.getIdNum()] = []
         self.isSorted = False
+
     def addGrade(self, student, grade):
         """Assumes: grade is a float
         Add grade to the list of grades for student"""
         try:
             self.grades[student.getIdNum()].append(grade)
-        except:
+        except KeyError:
             raise ValueError('Student not in mapping')
+
     def getGrades(self, student):
         """Return a list of grades for student"""
-        try: #return copy of student's grades
+        try:    #return copy of student's grades
             return self.grades[student.getIdNum()][:]
-        except:
+        except KeyError:
             raise ValueError('Student not in mapping')
+
     def getStudents(self):
         """Return a list of the students in the grade book"""
         if not self.isSorted:
             self.students.sort()
             self.isSorted = True
         return self.students[:] #return copy of list of students
+
+    def allStudents(self):
+        """returns a list of the studens in the grade book"""
+        if not self.isSorted:
+            self.students.sort()
+            self.isSorted = True
+        return self.students[:]
+
+    def gradeReport(course):
+        """assumes: course is of type grades"""
+        report = []
+        for s in course.allStudents():
+            tot = 0.0
+            numGrades = 0
+            for g in course.getGrades(s):
+                tot += g
+                numGrades += 1
+            try:
+                average = tot/numGrades
+                report.append(str(s) + '\'s mean grade is'
+                              + str(average))
+            except ZeroDivisionError:
+                report.append(str(s) + ' has no grades')
+        return '\n'.join(report)
